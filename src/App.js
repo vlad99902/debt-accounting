15010f28254e5ef4102acc53f045bef5f54568db
xylb-pro <churakin.kirill@gmail.com>
2020-12-02 11:57:52 +0300

    FUCK YOU

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.sass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <div className='main-block'>
+          <div className='left-block'></div>
+          <div className='right-block'></div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
