ce7b6daaab999fb2023a50999c6846e5430f5acb
xylb-pro <churakin.kirill@gmail.com>
2020-12-04 20:58:37 +0300

    PageHeader noComplete ubdate

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,26 +2,37 @@ import React from 'react'
 
 import '../styles/PageHeader.sass'
 
+import { Total } from '../components/Total'
+
+import debt from '../store/Debt'
+
 export const PageHeader = ({
   children,
   userName = 'kekw'
 }) => {
 
   return (
-    <div className="page-header">
+    <div className="wrapper">
       <div className="container">
-        <div className="inner">
+        <div className="page-header">
           <div className="page-header__logo">
             debt-accounting
           </div>
-          <div className="page-header__user">
-            {userName}
+          <div className="page-header__navigation">
+            <div className="page-header__balance">
+              <Total total={debt.allTotal}></Total>
+            </div>
+            <div className="page-header__user">
+              {userName}
+            </div>
+            <div className="page-header__settings">
+              settings
+            </div>
+            <div className="page-header__logout">
+              log out
+            </div>
           </div>
-          <div className="page-header__settings">
-            settings
-          </div>
-          <div className="">
-          </div>
+
         </div>
       </div>
     </div>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -57,51 +57,54 @@ export const MainPage = observer(() => {
   };
 
   return (
-    <div className="container">
+    <>
       <PageHeader>WASFDS</PageHeader>
-      <Total total={debt.allTotal} fw="500" fz="30px" />
-      <div className="lists">
-        <div className="lists__left">
-          <Total total={debt.oweTotal} />
-          <div className="lists__card">
-            <Card items={debt.oweList} />
+      <div className="container">
+
+        <Total total={debt.allTotal} fw="500" fz="30px" />
+        <div className="lists">
+          <div className="lists__left">
+            <Total total={debt.oweTotal} />
+            <div className="lists__card">
+              <Card items={debt.oweList} />
+            </div>
+          </div>
+
+          <div className="lists__right">
+            <Total total={debt.shouldTotal} />
+            <div className="lists__card">
+              <Card items={debt.shouldList} />
+            </div>
           </div>
         </div>
 
-        <div className="lists__right">
-          <Total total={debt.shouldTotal} />
-          <div className="lists__card">
-            <Card items={debt.shouldList} />
+        <div className="lists__buttons">
+          <div className="lists__button">
+            <Button text="Add" onClick={() => onClickAdd(true)} />
+          </div>
+
+          <div className="lists__button">
+            <Button text="Add" onClick={() => onClickAdd(false)} />
           </div>
         </div>
-      </div>
 
-      <div className="lists__buttons">
-        <div className="lists__button">
-          <Button text="Add" onClick={() => onClickAdd(true)} />
+        <div className="lists__inputs">
+          <input
+            type="text"
+            className="input lists__input"
+            placeholder="Title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <input
+            className="input lists__input"
+            type="number"
+            placeholder="Sum"
+            value={sum}
+            onChange={(event) => setSum(event.target.value)}
+          />
         </div>
-
-        <div className="lists__button">
-          <Button text="Add" onClick={() => onClickAdd(false)} />
-        </div>
       </div>
-
-      <div className="lists__inputs">
-        <input
-          type="text"
-          className="input lists__input"
-          placeholder="Title"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-        <input
-          className="input lists__input"
-          type="number"
-          placeholder="Sum"
-          value={sum}
-          onChange={(event) => setSum(event.target.value)}
-        />
-      </div>
-    </div>
+    </>
   );
 });
