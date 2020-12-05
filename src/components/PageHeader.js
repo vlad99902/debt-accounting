c47dd0285543402b8e5727f07c48831050136248
xylb-pro <churakin.kirill@gmail.com>
2020-12-05 08:11:48 +0300

    TWITCH SVG ICON WOW

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -15,6 +15,8 @@ export const PageHeader = observer(({ mb = '0px' }) => {
   const stl = [''];
   debt.allTotal < 0 ? stl.push('exc-red') : stl.push('exc-green');
 
+  let userName = debt.email.split('@').shift()
+
   return (
     <div className="wrapper" style={{ marginBottom: mb }}>
       <div className="container">
@@ -40,17 +42,25 @@ export const PageHeader = observer(({ mb = '0px' }) => {
             </div>
             <div className="page-header__nav-el page-header__user">
               <NavLink to="/settings" activeClassName="active">
-                {debt.email}
+                {userName}
               </NavLink>
             </div>
             <div className="page-header__nav-el page-header__logout">
-              <a href="/home" onClick={() => debt.logout()}>
-                log out
+              <a href="/home" onClick={() => debt.logout()} className="page-header__logout-inner">
+                <svg className="page-header__logout-icon">
+                  <g>
+                    <path d="M16 18h-4a2 2 0 01-2-2v-2h2v2h4V4h-4v2h-2V4a2 2 0 012-2h4a2 2 0 012 2v12a2 2 0 01-2 2z">
+                    </path>
+                    <path d="M7 5l1.5 1.5L6 9h8v2H6l2.5 2.5L7 15l-5-5 5-5z">
+                    </path>
+                  </g>
+                </svg>
+                <div className="page-header__logout-text">log out</div>
               </a>
             </div>
           </div>
         </nav>
       </div>
-    </div>
+    </div >
   );
 });
