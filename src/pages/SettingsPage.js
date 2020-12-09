9f3cd47520696276b81ea0047bbe164d05038d0f
vlad99902 <legkovv@mail.ru>
2020-12-09 05:13:01 +0300

    settings

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import { observer } from 'mobx-react-lite'
+import React from 'react';
+import { observer } from 'mobx-react-lite';
 
-import '../styles/SettingsPage.sass'
+import '../styles/SettingsPage.sass';
 
 export const SettingsPage = observer(() => {
-
   return (
     <>
-      <div>ПОШЕЛ НАХУЙ НЕ ЛЕЗЬ В НАСТРОЙКИ</div>
+      <div className="container">
+        <h1>Настройки</h1>
+      </div>
     </>
-  )
-})
+  );
+});
