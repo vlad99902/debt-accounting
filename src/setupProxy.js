9f593fe45032d8a1a68ba6cbd818fc1eb6236207
vlad99902 <legkovv@mail.ru>
2020-12-09 10:50:24 +0300

    some changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    proxy('/api/**', {
-      target: 'https://debt-accounting-server.herokuapp.com',
-    }),
-  );
-  app.use(
-    proxy('/otherApi/**', {
-      target: 'https://debt-accounting-server.herokuapp.com',
+    '/api',
+    createProxyMiddleware({
+      target: 'https://debt-accounting-server.herokuapp.com/',
+      changeOrigin: true,
     }),
   );
 };
