7cf507cd3c0ec0b27aeec52edd78260b00f04a6c
xylb-pro <churakin.kirill@gmail.com>
2020-12-18 18:20:08 +0300

    design settings

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ export const Header = ({
   fz = "26px",
   ta = "center",
   mb = "0px",
+  mt = "0px",
   children,
 }) => {
   const style = {
@@ -14,6 +15,7 @@ export const Header = ({
     fontSize: fz,
     textAlign: ta,
     marginBottom: mb,
+    marginTop: mt,
   };
   return (
     <h1 className="header" style={style}>
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -2,18 +2,24 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 
 import debt from "../store/Debt";
+import { Header } from "../components/Header/Header";
+import { Button } from "../components/Button/Button";
 
 import "./SettingsPage.sass";
 
 export const SettingsPage = observer(() => {
   return (
-    <>
-      <div className="container">
-        <h1>Settings</h1>
-        <a href="/home" onClick={() => debt.logout()}>
-          <div style={{ textDecoration: "underline" }}>log out</div>
-        </a>
+    <div className="container container--padding-vertical">
+      <div className="content">
+        <div className="content__header">
+          <Header fw="600" fz="26px" ta="left">
+            Settings
+          </Header>
+        </div>
+        <div className="content__log-out">
+          <Button onClick={() => debt.logout()}>log out</Button>
+        </div>
       </div>
-    </>
+    </div>
   );
 });
